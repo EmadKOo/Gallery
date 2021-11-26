3b1996bd2439c38eaf8cbe773bd7b077f303dc07
emad <emad.hetata97@gmail.com>
2021-11-26 20:42:34 +0000

    select image from gallery

--- app/src/main/java/com/example/studio/presentaion/fragments/HomeFragment.java
+++ app/src/main/java/com/example/studio/presentaion/fragments/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.studio.presentaion.fragments;
 
+import static android.app.Activity.RESULT_OK;
 import static com.example.studio.utility.Constants.CAMERA_PERMISSION_CODE;
+import static com.example.studio.utility.Constants.CAPTURED_IMAGE;
 import static com.example.studio.utility.Constants.GALLERY_PERMISSION_CODE;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -13,6 +18,8 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,14 +28,19 @@ import android.widget.Toast;
 import com.example.studio.R;
 import com.example.studio.databinding.FragmentHomeBinding;
 
+import java.io.File;
+
+import javax.xml.transform.Result;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
 @AndroidEntryPoint
 public class HomeFragment extends Fragment {
-
+    private static final String TAG = "EMADADADA";
     private FragmentHomeBinding mBinding;
-
+    private File imageFile;
+    private Uri imageUri;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +61,9 @@ public class HomeFragment extends Fragment {
 
     private void handleViews() {
         mBinding.takePhoto.setOnClickListener(view -> {
-            hasCameraPermission();
+            if (hasCameraPermission()){
+                pickPhoto();
+            }
         });
         mBinding.openGallery.setOnClickListener(view -> {
             hasGalleryPermission();
@@ -73,12 +87,23 @@ public class HomeFragment extends Fragment {
             return true;
     }
 
+    private void pickPhoto() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "NewPic");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image To Text");
+        imageUri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, CAPTURED_IMAGE);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(getActivity(), "camera permission granted", Toast.LENGTH_LONG).show();
+                pickPhoto();
             } else {
                 Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
             }
@@ -89,5 +114,14 @@ public class HomeFragment extends Fragment {
                 Toast.makeText(getActivity(), "gallery permission denied", Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: outside " + resultCode + " "+ requestCode);
+        Toast.makeText(getActivity(), "Eadda", Toast.LENGTH_SHORT).show();
+        if (requestCode== CAPTURED_IMAGE && resultCode== RESULT_OK )
+            mBinding.selectedImage.setImageURI(imageUri);
     }
 }
--- app/src/main/java/com/example/studio/utility/Constants.java
+++ app/src/main/java/com/example/studio/utility/Constants.java
@@ -3,5 +3,6 @@ package com.example.studio.utility;
 public class Constants {
     public static int CAMERA_PERMISSION_CODE = 1045;
     public static int GALLERY_PERMISSION_CODE = 1046;
+    public static int CAPTURED_IMAGE=1047;
 
 }
